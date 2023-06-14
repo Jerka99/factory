de15edaebfdae1c1eb9896be1d12b79a84d0d94f
unknown <antoniojerka@gmail.com>
2023-06-14 23:56:40 +0200

    wrap

--- main.js
+++ main.js
@@ -7,12 +7,14 @@ $(function() {
   console.log( "ready!" );
   let leftArrow = $("#second-row #buttons button:first");
   let rightArrow = $("#second-row #buttons button:last");
-
+  let firstRow = [];
+  let secondRow = [];
+  let temp = null;
 
   function handler(x){
     const color = $(this).children().attr("src").substring(15,19);
     const arrow = $(this).children().attr("src").substring(20,21);
-    console.log($(this).css("border", "solid 1px darkgray"))    
+
     if(arrow == "l"){
     $(this).css("border", `solid 1px ${color == "blue" ? "darkgray" : "#134880"}`);
     $(this).children().attr("src",`./assets/arrow-${color == "blue" ? "gray" : "blue"}-left.png`);
@@ -33,13 +35,57 @@ $(function() {
     El2:"#second-row"
   }
 
+  const sumFun = (x) =>{
+    const nodelistToArray = Array.prototype.slice.call($(`${x} #images-holder img`));
+    let sum = 0;
+    nodelistToArray.forEach((el)=>{
+     sum += el.width + 10;
+   })
+    return sum
+  }
+
+const fun = (x) =>{
+
+  console.log(temp, x)
+
+  if(temp != x){
+  firstRow = [];
+  secondRow = [];
+
   Object.keys(images).forEach((el, index)=>{ 
-    if(index < 5){
-            return $("#first-row #images-holder #images").append(`<img src=${el} alt="">`)
+    if(index > x ?? -1){
+      firstRow.push(`<img src=${el} alt="">`)
           }
-        else {
-          return $("#second-row #images-holder #images").append(`<img src=${el} alt="">`)
-      }})
+       else {
+        secondRow.push(`<img src=${el} alt="">`)
+      }
+    })     
+      $("#first-row #images-holder #images").html(firstRow.join(""))
+      $("#second-row #images-holder #images").html(secondRow.join(""))
+
+
+    setTimeout(() => {
+        if(x==3){
+        $("#first-row #images-holder").css("width",`${sumFun("#first-row")}px`)
+        $("#second-row #images-holder").css("width",`${sumFun("#second-row")}px`)
+      }
+      else{
+        $("#first-row #images-holder").css("width",`${sumFun("#first-row")}px`)
+      }
+      }, 200);
+   temp = x;   
+  }
+  }
+
+      $( window ).on( "resize load", function() {     
+                
+        if($(window).width() < 1600){
+          fun(3)
+        }
+        else{
+          fun(-1)
+        }
+      } );
 
 const turnOffButtons = () =>{
   leftArrow.off("click",left);
