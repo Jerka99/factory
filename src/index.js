59adb03bdbbfa79c5910aa5d864ffa3ad3bb4800
unknown <antoniojerka@gmail.com>
2023-06-21 17:54:34 +0200

    add

--- src/index.js
+++ src/index.js
@@ -1,67 +1,64 @@
+$(function () {
 
- 
-$(function() {
-  console.log( "ready!" );
-  
-
-  let leftArrow = $("#second-row #buttons button:first");
-  let rightArrow = $("#second-row #buttons button:last");
-
+  let leftArrow = $("#left-arrow");
+  let rightArrow = $("#right-arrow");
 
   const obj = {
-    El1:"#first-row",
-    El2:"#second-row"
-  }
+    El1: "#first-row",
+    El2: "#second-row",
+  };
 
-
-const turnOffButtons = () =>{
-  leftArrow.off("click",left);
-  rightArrow.off("click", right);
+  const turnOffButtons = () => {
+    leftArrow.off("click", left);
+    rightArrow.off("click", right);
     setTimeout(() => {
-      leftArrow.on("click",left);
-      rightArrow.on("click", right)
-    }, 250); 
-}
+      leftArrow.on("click", left);
+      rightArrow.on("click", right);
+    }, 250);
+  };
 
+  const rightScroll = (x) => {
+    $(`${obj[x]} .images`).removeClass("left");
+    const El = $(`${obj[x]} .images img`).last();
 
- const rightScroll = (x) =>{
-  $(`${obj[x]} #images`).removeClass("left")
-  const El = $(`${obj[x]} #images img`).last();
+    El.clone().prependTo(`${obj[x]} .images`);
 
-   El.clone().prependTo(`${obj[x]} #images`);
+    $(`${obj[x]} .images`).animate(
+      { right: -El.width() - 10 },
+      240,
+      function () {
+        El.remove();
+        $(this).attr("style", "right: 0px");
+      }
+    );
+  };
 
-    $(`${obj[x]} #images`).animate({right: -El.width() - 10},240, function() {
-      El.remove();
-      $(this).attr('style','right: 0px');
-    });
-  }
+  const leftScroll = (x) => {
+    $(`${obj[x]} .images`).addClass("left");
+    const El = $($(`${obj[x]} .images img`)).first();
+    El.clone().appendTo($(`${obj[x]} .images`));
+    $(`${obj[x]} .images`).animate(
+      { right: El.width() + 10 },
+      240,
+      function () {
+        El.remove();
+        $(this).attr("style", "right: 0px");
+      }
+    );
+  };
 
-  const leftScroll = (x) =>{
-    $(`${obj[x]} #images`).addClass("left")
-    const El = $(`${obj[x]} #images img`).first();
-
-    El.clone().appendTo(`${obj[x]} #images`);
-
-    $(`${obj[x]} #images`).animate({right: El.width() + 10},240, function() {
-      El.remove();
-      $(this).attr('style','right: 0px');
-    });
-  }
-
-  const right = () =>{
+  const right = () => {
     turnOffButtons();
     rightScroll("El1");
     rightScroll("El2");
-  }
+  };
 
-  const left = () =>{
+  const left = () => {
     turnOffButtons();
     leftScroll("El1");
     leftScroll("El2");
-  }
+  };
 
-  rightArrow.on("click",right)
-  leftArrow.on("click",left)
- 
+  rightArrow.on("click", right);
+  leftArrow.on("click", left);
 });
-
